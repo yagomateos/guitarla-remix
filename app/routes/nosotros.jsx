3c80bb8d00f6b3c163f9b6fa17e4d201d354b998
Yago <yagomateos@hotmail.com>
2023-01-15 10:00:46 +0100

    GuitarLA-Remix

---
+++ app/routes/nosotros.jsx
@@ -0,0 +1,55 @@
+import imagen from '../../public/img/nosotros.jpg'
+import styles from '~/styles/nosotros.css'
+
+export function meta(){
+    return(
+        {
+            title: 'GuitarraLA - Sobre Nosotros',
+            description:'Venta de guitarras,blog de música'
+        }
+    )
+}
+
+export function links(){
+    return [
+        {
+            rel: 'stylesheet',
+            href: styles
+        },
+        {
+            rel: 'preload',
+            href: imagen,
+            as:'image'
+        },
+    ]
+
+}
+
+function Nosotros() {
+    return (
+      <main className="contenedor nosotros">
+      <h2 className="heading">Nosotros</h2>
+
+      <div className="contenido">
+        <img src ={imagen} alt="imagen sobre nosotros" />
+        <div>
+            <p>Duis ultricies commodo nunc. Phasellus a sagittis felis, sit amet volutpat nulla.
+             Donec eu vestibulum neque. Mauris iaculis nunc eget mauris fringilla, vitae viverra
+            purus tincidunt. Aenean tempus velit a justo posuere, at efficitur orci eleifend.
+             Mauris eget dui in ipsum ultrices facilisis vel ac massa. Donec mollis magna metus,
+              non ultrices lorem convallis sed.
+               </p>
+
+                   <p>Duis ultricies commodo nunc. Phasellus a sagittis felis, sit amet volutpat nulla.
+             Donec eu vestibulum neque. Mauris iaculis nunc eget mauris fringilla, vitae viverra
+            purus tincidunt. Aenean tempus velit a justo posuere, at efficitur orci eleifend.
+             Mauris eget dui in ipsum ultrices facilisis vel ac massa. Donec mollis magna metus,
+              non ultrices lorem convallis sed. 
+               </p>
+        </div>
+      </div>
+      </main>
+    )
+  }
+  
+  export default Nosotros
---
+++ app/routes/tienda.jsx
@@ -0,0 +1,56 @@
+import { useLoaderData } from '@remix-run/react'
+import { getGuitarras } from '~/models/guitarras.server'
+import Guitarra from '~/components/guitarra'
+import styles from '~/styles/guitarras.css'
+
+export function meta() {
+  return {
+      
+          title: 'GuitarLA - Tienda de Guitarras',
+          description: 'GuitarLA - Nuestra colección de Guitarras'
+      }
+ 
+    }
+
+
+
+export function links(){
+  return [
+      {
+          rel: 'stylesheet',
+          href: styles
+      },
+ 
+  ]
+
+}
+
+export async  function loader() {
+const guitarras = await getGuitarras()
+return guitarras.data
+}
+
+function Tienda() {
+
+  const guitarras = useLoaderData()
+
+    return (
+      <main className='contenedor'>
+        <h2 className='heading'>Nuestra Colección</h2>
+
+        {guitarras.length &&(
+          <div className='guitarras-grid'>
+            {guitarras.map( guitarra =>(
+              <Guitarra
+              key={guitarra?.id}
+                guitarra={guitarra?.attributes}
+              />
+            ))}
+          </div>
+        )}
+      </main>
+     
+    )
+  }
+  
+  export default Tienda
